e0610b9ce3426e57db639002812c17b07549abc4
Abdülsamed Türker <abdulsamed.turker@outlook.de>
2024-12-12 18:17:41 +0100

    Add localforage/storage clear on app start up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import localforage from 'localforage';
 
 import HelloPage from "./pages/HelloPage";
 import CalibrationPage from "./pages/CalibrationPage";
@@ -11,6 +12,22 @@ import './App.css';
 function App() {
   const [isTrackingEye, setIsTrackingEye] = useState(false);
   const [eyeTrackingData, setEyeTrackingData] = useState([]);
+
+  useEffect(() => {
+    localStorage.clear();
+    sessionStorage.clear();
+    localforage.clear().then(() => {
+      console.log('LocalForage data has been cleared.');
+    }).catch((err) => {
+      console.error('Error clearing LocalForage data:', err);
+    });
+    
+    const cookies = document.cookie.split("; ");
+    for (const cookie of cookies) {
+      const [name] = cookie.split("=");
+      document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+    }
+  }, []);
 
   return (
     <>
