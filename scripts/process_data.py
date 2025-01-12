fe0b6f05f44b7ec3ae0b33bd46a45204ece073f1
Abus00 <abdulsamed.turker@outlook.de>
2025-01-12 20:48:08 +0100

    certificates removed from git history

---
+++ scripts/process_data.py
@@ -0,0 +1,4 @@
+'''
+    This script can be used to process the data that is stored within the database.db file.
+    However, this prototype does not include any data processing, which is why this script is empty.
+'''
